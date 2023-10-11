66de51ad0134c62f04f079d8662f871da0d708ba
yangxdev <yangxdev@gmail.com>
2023-10-11 23:14:29 +0200

    text update

--- app/CareerObjective.jsx
+++ app/CareerObjective.jsx
@@ -5,7 +5,7 @@ const CareerObjective = () => (
         <h1 className="uppercase font-bold text-xl mb-1">Career Objective</h1>
         <Divider />
         <h3 className="card-right-section1-career-objective-description text-xs">
-            <strong>Computer Science</strong> graduate and <strong>detail-oriented</strong> front-end developer with <strong>over a year of experience</strong> in building websites and web applications. Aiming to employ my problem-solving skills and attention to details in a challenging position at a fast-paced company.
+            <strong>Computer Science</strong> graduate and <strong>detail-oriented</strong> full-stack developer with <strong>over a year of experience</strong> in building websites and web applications. Aiming to employ my problem-solving skills and attention to details in a challenging position at a fast-paced company.
         </h3>
     </div>
 );
